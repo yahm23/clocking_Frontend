fb671d1e9261217b3b48deff00adbdcca729be95
yahm23 <yahmed1996a@gmail.com>
2020-05-28 12:44:46 +0100

    first commit

---
+++ src/comps/addStaff.js
@@ -0,0 +1,18 @@
+import React  from 'react'
+
+export const AddStaff= ()=> {
+    
+
+    return (
+        <div>
+            <form>
+                <label>Full Name:</label>
+                <input></input>
+            </form>
+        </div>
+    )
+
+    
+}
+
+export default AddStaff
---
+++ src/comps/dateTime.js
@@ -0,0 +1,26 @@
+import  React, { useState , useEffect } from 'react'
+
+export const DateTime = () => {
+
+    var [date,setDate] = useState(new Date());
+    
+    useEffect(() => {
+        
+        var timer = setInterval(()=>setDate(new Date()), 1000 )
+
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    
+    });
+
+    return(
+        <div>
+            <p> Time : {date.toLocaleTimeString()}</p>
+        </div>
+    )
+}
+
+export default DateTime
+
+
